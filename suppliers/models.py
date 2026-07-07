6839367824aa38ad7811cbd1e4e0205aee723934
patricia Lydie Elom <patricialydieelom@gmail.com>
2026-07-07 12:06:56 +0100

    gestion commerciale

---
+++ suppliers/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=50, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
